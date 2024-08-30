6f7601d0a182394463fc008ec619cf512112487d
richardbattles <rickyjbattles@gmail.com>
2024-08-30 08:34:46 -0400

    Added dropout & pooling layers to prelude.

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,9 @@
 pub use crate::layers::{
         layer::Layer,
         dense_layer::DenseLayer,
+        convolutional_layer::*,
         softmax_layer::SoftmaxLayer,
         reshaping_layer::ReshapingLayer,
-        convolutional_layer::*};
+        dropout_layer::*,
+        pooling_layer::*,
+};
